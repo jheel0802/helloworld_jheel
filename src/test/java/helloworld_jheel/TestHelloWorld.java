146025a24aa9c4477c3858c322fc86318b60cd40
jheel0802 <jheel0802@gmail.com>
2023-03-29 21:38:56 +0530

    initial commit

---
+++ src/test/java/helloworld_jheel/TestHelloWorld.java
@@ -0,0 +1,11 @@
+package helloworld_jheel;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+
+	@Test
+	public void testHelloWorld() {
+		System.out.println(".........printing Hello World!!");
+	}
+}
